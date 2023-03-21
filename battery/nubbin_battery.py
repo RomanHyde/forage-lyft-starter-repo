7c3de8c412bae1bbd9446e12ede7bb0cdbc68057
romanhyde <romanhyde1@gmail.com>
2023-03-21 14:54:43 +0000

    correct date logic

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -4,11 +4,12 @@ from utils import add_years_to_date
 
 class NubbinBattery(Battery):
     def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date
         self.current_date = current_date
+        self.last_service_date = last_service_date
 
     def needs_service(self):
-        if self.current_date + self.last_service_date > 4:
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 4)
+        if date_which_battery_should_be_serviced_by < self.current_date:
             return True
         else:
             return False
