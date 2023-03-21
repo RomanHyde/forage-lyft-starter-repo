510d7eb636e53d8fa101667beb8b6baa74a4df1b
romanhyde <romanhyde1@gmail.com>
2023-03-21 14:10:09 +0000

    initial refactor

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,13 @@
+from battery.battery import Battery
+
+
+class Nubbin_Battery(Battery):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        if self.current_date + self.last_service_date > 4:
+            return True
+        else:
+            return False
